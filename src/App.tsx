6bae01918b4e1513a57c2bc194952e86ce7644df
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2022-10-11 15:35:44 +0800

    Update Auth0 to include SMS

--- src/App.tsx
+++ src/App.tsx
@@ -1,213 +1,278 @@
-import {useEffect, useState} from 'react'
-import {Web3AuthCore} from '@web3auth/core'
+import { useEffect, useState } from 'react';
+import { Web3AuthCore } from '@web3auth/core';
 import {
-  WALLET_ADAPTERS,
-  CHAIN_NAMESPACES,
-  SafeEventEmitterProvider,
-} from '@web3auth/base'
-import {OpenloginAdapter} from '@web3auth/openlogin-adapter'
-import RPC from './evm'
-import './App.css'
+	WALLET_ADAPTERS,
+	CHAIN_NAMESPACES,
+	SafeEventEmitterProvider,
+} from '@web3auth/base';
+import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
+import RPC from './evm';
+import './App.css';
 
 const clientId =
-  'BBWLCT1dIp57_-rJQwfMTTWqZgncOKu56b1TSqyLptpwSKrEndkCerKzgJ7aZ87_jviI5oLUJVCt-_mTqBJUSyQ' // get from https://dashboard.web3auth.io
+	'BBWLCT1dIp57_-rJQwfMTTWqZgncOKu56b1TSqyLptpwSKrEndkCerKzgJ7aZ87_jviI5oLUJVCt-_mTqBJUSyQ'; // get from https://dashboard.web3auth.io
 
 function App() {
-  const [web3auth, setWeb3auth] = useState<Web3AuthCore | null>(null)
-  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(
-    null,
-  )
+	const [web3auth, setWeb3auth] = useState<Web3AuthCore | null>(null);
+	const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(
+		null,
+	);
 
-  useEffect(() => {
-    const init = async () => {
-      try {
-        const web3auth = new Web3AuthCore({
-          chainConfig: {
-            chainNamespace: CHAIN_NAMESPACES.EIP155,
-            chainId: '0x1',
-          },
-        })
+	useEffect(() => {
+		const init = async () => {
+			try {
+				const web3auth = new Web3AuthCore({
+					chainConfig: {
+						chainNamespace: CHAIN_NAMESPACES.EIP155,
+						chainId: '0x1',
+					},
+				});
 
-        const openloginAdapter = new OpenloginAdapter({
-          adapterSettings: {
-            clientId,
-            network: 'testnet',
-            uxMode: 'popup',
-            loginConfig: {
-              jwt: {
-                name: 'Web3Auth-Auth0-JWT',
-                verifier: 'web3auth-auth0-alam',
-                typeOfLogin: 'jwt',
-                clientId: '294QRkchfq2YaXUbPri7D6PH7xzHgQMT',
-              },
-            },
-          },
-        })
-        web3auth.configureAdapter(openloginAdapter)
-        setWeb3auth(web3auth)
+				const openloginAdapter = new OpenloginAdapter({
+					adapterSettings: {
+						clientId,
+						network: 'testnet',
+						uxMode: 'popup',
+						loginConfig: {
+							jwt: {
+								name: 'Web3Auth-Auth0-JWT',
+								verifier: 'web3auth-auth0-alam',
+								typeOfLogin: 'jwt',
+								clientId: '294QRkchfq2YaXUbPri7D6PH7xzHgQMT',
+							},
+						},
+					},
+				});
+				web3auth.configureAdapter(openloginAdapter);
+				await web3auth.init();
+				setWeb3auth(web3auth);
 
-        await web3auth.init()
-      } catch (error) {
-        console.error(error)
-      }
-    }
+				if (web3auth.provider) {
+					setProvider(web3auth.provider);
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		};
 
-    init()
-  }, [])
+		init();
+	}, []);
 
-  const login = async () => {
-    if (!web3auth) {
-      uiConsole('web3auth not initialized yet')
-      return
-    }
-    const web3authProvider = await web3auth.connectTo(
-      WALLET_ADAPTERS.OPENLOGIN,
-      {
-        relogin: true,
-        loginProvider: 'jwt',
-        extraLoginOptions: {
-          domain: 'https://shahbaz-torus.us.auth0.com',
-          verifierIdField: 'sub',
-        },
-      },
-    )
-    setProvider(web3authProvider)
-  }
+	const login = async () => {
+		if (!web3auth) {
+			uiConsole('web3auth not initialized yet');
+			return;
+		}
+		const web3authProvider = await web3auth.connectTo(
+			WALLET_ADAPTERS.OPENLOGIN,
+			{
+				relogin: true,
+				loginProvider: 'jwt',
+				extraLoginOptions: {
+					domain: 'https://shahbaz-torus.us.auth0.com',
+					verifierIdField: 'sub',
+				},
+			},
+		);
+		setProvider(web3authProvider);
+	};
 
-  const getUserInfo = async () => {
-    if (!web3auth) {
-      uiConsole('web3auth not initialized yet')
-      return
-    }
-    const user = await web3auth.getUserInfo()
-    uiConsole(user)
-  }
+	const authenticateUser = async () => {
+		if (!web3auth) {
+			uiConsole('web3auth not initialized yet');
+			return;
+		}
+		const idToken = await web3auth.authenticateUser();
+		uiConsole(idToken);
+	};
 
-  const logout = async () => {
-    if (!web3auth) {
-      uiConsole('web3auth not initialized yet')
-      return
-    }
-    await web3auth.logout()
-    setProvider(null)
-  }
+	const parseToken = async () => {
+		const idToken = await web3auth?.authenticateUser();
+		console.log(idToken?.idToken);
+		const base64Url = idToken?.idToken.split('.')[1];
+		const base64 = base64Url?.replace('-', '+').replace('_', '/');
+		const result = JSON.parse(window.atob(base64 || ''));
+		uiConsole(result);
+	};
 
-  const getAccounts = async () => {
-    if (!provider) {
-      uiConsole('provider not initialized yet')
-      return
-    }
-    const rpc = new RPC(provider)
-    const userAccount = await rpc.getAccounts()
-    uiConsole(userAccount)
-  }
+	const getUserInfo = async () => {
+		if (!web3auth) {
+			uiConsole('web3auth not initialized yet');
+			return;
+		}
+		const user = await web3auth.getUserInfo();
+		uiConsole(user);
+	};
 
-  const getBalance = async () => {
-    if (!provider) {
-      uiConsole('provider not initialized yet')
-      return
-    }
-    const rpc = new RPC(provider)
-    const balance = await rpc.getBalance()
-    uiConsole(balance)
-  }
+	const logout = async () => {
+		if (!web3auth) {
+			uiConsole('web3auth not initialized yet');
+			return;
+		}
+		await web3auth.logout();
+		setProvider(null);
+	};
 
-  const signMessage = async () => {
-    if (!provider) {
-      uiConsole('provider not initialized yet')
-      return
-    }
-    const rpc = new RPC(provider)
-    const result = await rpc.signMessage()
-    uiConsole(result)
-  }
+	const getAccounts = async () => {
+		if (!provider) {
+			uiConsole('provider not initialized yet');
+			return;
+		}
+		const rpc = new RPC(provider);
+		const userAccount = await rpc.getAccounts();
+		uiConsole(userAccount);
+	};
 
-  const signTransaction = async () => {
-    if (!provider) {
-      uiConsole('provider not initialized yet')
-      return
-    }
-    const rpc = new RPC(provider)
-    const result = await rpc.signTransaction()
-    uiConsole(result)
-  }
+	const getBalance = async () => {
+		if (!provider) {
+			uiConsole('provider not initialized yet');
+			return;
+		}
+		const rpc = new RPC(provider);
+		const balance = await rpc.getBalance();
+		uiConsole(balance);
+	};
 
-  const sendTransaction = async () => {
-    if (!provider) {
-      uiConsole('provider not initialized yet')
-      return
-    }
-    const rpc = new RPC(provider)
-    const result = await rpc.signAndSendTransaction()
-    uiConsole(result)
-  }
+	const signMessage = async () => {
+		if (!provider) {
+			uiConsole('provider not initialized yet');
+			return;
+		}
+		const rpc = new RPC(provider);
+		const result = await rpc.signMessage();
+		uiConsole(result);
+	};
 
-  function uiConsole(...args: any[]): void {
-    const el = document.querySelector('#console>p')
-    if (el) {
-      el.innerHTML = JSON.stringify(args || {}, null, 2)
-    }
-  }
+	const signTransaction = async () => {
+		if (!provider) {
+			uiConsole('provider not initialized yet');
+			return;
+		}
+		const rpc = new RPC(provider);
+		const result = await rpc.signTransaction();
+		uiConsole(result);
+	};
 
-  const loggedInView = (
-    <>
-      <button onClick={getUserInfo} className="card">
-        Get User Info
-      </button>
-      <button onClick={getAccounts} className="card">
-        Get Accounts
-      </button>
-      <button onClick={getBalance} className="card">
-        Get Balance
-      </button>
-      <button onClick={signMessage} className="card">
-        Sign Message
-      </button>
-      <button onClick={signTransaction} className="card">
-        Sign Transaction
-      </button>
-      <button onClick={sendTransaction} className="card">
-        Send Transaction
-      </button>
+	const sendTransaction = async () => {
+		if (!provider) {
+			uiConsole('provider not initialized yet');
+			return;
+		}
+		const rpc = new RPC(provider);
+		const result = await rpc.sendTransaction();
+		uiConsole(result);
+	};
 
-      <button onClick={logout} className="card">
-        Log Out
-      </button>
+	const getChainId = async () => {
+		if (!provider) {
+			uiConsole('provider not initialized yet');
+			return;
+		}
+		const rpc = new RPC(provider);
+		const chainId = await rpc.getChainId();
+		uiConsole(chainId);
+	};
 
-      <div id="console" style={{whiteSpace: 'pre-line'}}>
-        <p style={{whiteSpace: 'pre-line'}}></p>
-      </div>
-    </>
-  )
+	function uiConsole(...args: any[]): void {
+		const el = document.querySelector('#console>p');
+		if (el) {
+			el.innerHTML = JSON.stringify(args || {}, null, 2);
+		}
+	}
 
-  const unloggedInView = (
-    <button onClick={login} className="card">
-      Login
-    </button>
-  )
+	const loggedInView = (
+		<>
+			<div className='flex-container'>
+				<div>
+					<button onClick={getUserInfo} className='card'>
+						Get User Info
+					</button>
+				</div>
+				<div>
+					<button onClick={authenticateUser} className='card'>
+						Get idToken
+					</button>
+				</div>
+				<div>
+					<button onClick={parseToken} className='card'>
+						Parse idToken
+					</button>
+				</div>
+				<div>
+					<button onClick={getChainId} className='card'>
+						Get Chain ID
+					</button>
+				</div>
+				<div>
+					<button onClick={getAccounts} className='card'>
+						Get ETH Account
+					</button>
+				</div>
+				<div>
+					<button onClick={getBalance} className='card'>
+						Get Balance
+					</button>
+				</div>
+				<div>
+					<button onClick={signMessage} className='card'>
+						Sign Message
+					</button>
+				</div>
+				<div>
+					<button onClick={signTransaction} className='card'>
+						Sign Transaction
+					</button>
+				</div>
+				<div>
+					<button onClick={sendTransaction} className='card'>
+						Send Transaction
+					</button>
+				</div>
+				<div>
+					<button onClick={logout} className='card'>
+						Log Out
+					</button>
+				</div>
+			</div>
+			<div id='console' style={{ whiteSpace: 'pre-line' }}>
+				<p style={{ whiteSpace: 'pre-line' }} />
+			</div>
+		</>
+	);
 
-  return (
-    <div className="container">
-      <h1 className="title">
-        <a target="_blank" href="http://web3auth.io/" rel="noreferrer">
-          Web3Auth
-        </a>{' '}
-        & ReactJS Example using Auth0
-      </h1>
+	const unloggedInView = (
+		<button disabled={!web3auth} onClick={login} className='card'>
+			{web3auth ? 'Login' : 'Loading...'}
+		</button>
+	);
 
-      <div className="grid">{provider ? loggedInView : unloggedInView}</div>
+	return (
+		<div className='container'>
+			<h1 className='title'>
+				<a target='_blank' href='http://web3auth.io/' rel='noreferrer'>
+					Web3Auth
+				</a>{' '}
+				& ReactJS Example using Auth0
+			</h1>
 
-      <footer className="footer">
-        <a
-          href="https://github.com/Web3Auth/examples/tree/master/auth0-react-example"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Source code
-        </a>
-      </footer>
-    </div>
-  )
+			<h4 className='sub-title'>Google & SMS Login</h4>
+
+			<div className='grid'>{provider ? loggedInView : unloggedInView}</div>
+
+			<footer className='footer'>
+				<a
+					href='https://github.com/shahbaz17/web3auth-auth0-react-demo'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Source code
+				</a>
+				<p>
+					Note: For testing, SMS login only works with +65 and +91 numbers.{' '}
+				</p>
+			</footer>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/evm.ts
+++ src/evm.ts
@@ -1,86 +1,124 @@
-import type {SafeEventEmitterProvider} from '@web3auth/base'
-import Web3 from 'web3'
+import type { SafeEventEmitterProvider } from '@web3auth/base';
+import Web3 from 'web3';
 
 export default class EthereumRpc {
-  private provider: SafeEventEmitterProvider
+	private provider: SafeEventEmitterProvider;
 
-  constructor(provider: SafeEventEmitterProvider) {
-    this.provider = provider
-  }
-  async getAccounts(): Promise<string[]> {
-    try {
-      const web3 = new Web3(this.provider as any)
-      const accounts = await web3.eth.getAccounts()
-      return accounts
-    } catch (error: unknown) {
-      return error as string[]
-    }
-  }
+	constructor(provider: SafeEventEmitterProvider) {
+		this.provider = provider;
+	}
 
-  async getBalance(): Promise<string> {
-    try {
-      const web3 = new Web3(this.provider as any)
-      const accounts = await web3.eth.getAccounts()
-      const balance = await web3.eth.getBalance(accounts[0])
-      return balance
-    } catch (error) {
-      return error as string
-    }
-  }
+	async getChainId(): Promise<string> {
+		try {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			const web3 = new Web3(this.provider as any);
 
-  async signMessage() {
-    try {
-      const web3 = new Web3(this.provider as any)
-      const accounts = await web3.eth.getAccounts()
-      const message =
-        '0x47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad'
-      ;(web3.currentProvider as any)?.send(
-        {
-          method: 'eth_sign',
-          params: [accounts[0], message],
-          from: accounts[0],
-        },
-        (err: Error, result: any) => {
-          if (err) {
-            return console.error(err)
-          }
-          return result
-        },
-      )
-    } catch (error) {
-      return error as string
-    }
-    return
-  }
+			// Get the connected Chain's ID
+			const chainId = await web3.eth.getChainId();
 
-  async signTransaction(): Promise<string> {
-    try {
-      const web3 = new Web3(this.provider as any)
-      const accounts = await web3.eth.getAccounts()
-      const txRes = await web3.eth.signTransaction({
-        from: accounts[0],
-        to: accounts[0],
-        value: web3.utils.toWei('0.01'),
-      })
-      return txRes.raw
-    } catch (error) {
-      return error as string
-    }
-  }
+			return chainId.toString();
+		} catch (error) {
+			return error as string;
+		}
+	}
 
-  async signAndSendTransaction(): Promise<string> {
-    try {
-      const web3 = new Web3(this.provider as any)
-      const accounts = await web3.eth.getAccounts()
+	async getAccounts(): Promise<string[]> {
+		try {
+			const web3 = new Web3(this.provider as any);
+			const accounts = await web3.eth.getAccounts();
+			return accounts;
+		} catch (error: unknown) {
+			return error as string[];
+		}
+	}
 
-      const txRes = await web3.eth.sendTransaction({
-        from: accounts[0],
-        to: accounts[0],
-        value: web3.utils.toWei('0.01'),
-      })
-      return txRes.transactionHash
-    } catch (error) {
-      return error as string
-    }
-  }
+	async getBalance(): Promise<string> {
+		try {
+			const web3 = new Web3(this.provider as any);
+			const accounts = await web3.eth.getAccounts();
+			const balance = await web3.eth.getBalance(accounts[0]);
+			return balance;
+		} catch (error) {
+			return error as string;
+		}
+	}
+
+	async signMessage() {
+		try {
+			const web3 = new Web3(this.provider as any);
+
+			// Get user's Ethereum public address
+			const account = (await web3.eth.getAccounts())[0];
+
+			// Message
+			const message = 'Hello MPC, bye bye seedphrase';
+
+			const typedMessage = [
+				{
+					type: 'string',
+					name: 'message',
+					value: message,
+				},
+			];
+			const params = [JSON.stringify(typedMessage), account];
+			const method = 'eth_signTypedData';
+			const signedMessage = await this.provider.request({
+				method,
+				params,
+			});
+
+			return signedMessage as string;
+		} catch (error) {
+			return error as string;
+		}
+	}
+
+	async signTransaction(): Promise<any> {
+		try {
+			const web3 = new Web3(this.provider as any);
+
+			const fromAddress = (await web3.eth.getAccounts())[0];
+
+			const destination = fromAddress;
+
+			const amount = web3.utils.toWei('0.00001'); // Convert 1 ether to wei
+
+			// Submit transaction to the blockchain and wait for it to be mined
+			const receipt = await web3.eth.signTransaction({
+				from: fromAddress,
+				to: destination,
+				value: amount,
+				maxPriorityFeePerGas: '5000000000', // Max priority fee per gas
+				maxFeePerGas: '6000000000000', // Max fee per gas
+			});
+
+			return receipt;
+		} catch (error) {
+			return error as string;
+		}
+	}
+
+	async sendTransaction(): Promise<any> {
+		try {
+			const web3 = new Web3(this.provider as any);
+
+			// Get user's Ethereum public address
+			const fromAddress = (await web3.eth.getAccounts())[0];
+			const destination = fromAddress;
+
+			const amount = web3.utils.toWei('0.001'); // Convert 1 ether to wei
+
+			// Submit transaction to the blockchain and wait for it to be mined
+			const receipt = await web3.eth.sendTransaction({
+				from: fromAddress,
+				to: destination,
+				value: amount,
+				chainId: 5,
+			});
+
+			return receipt;
+		} catch (error) {
+			return error as string;
+		}
+	}
 }
